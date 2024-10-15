e3164672148fd7d8c475cb932cca1703e1d6e7e3
Sleepinr-frog <vdavtinv@gmail.com>
2024-10-16 00:55:12 +0400

    Adding FS-10

---
+++ FS-10/copy.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main (int argc, char** argv) {
+	if (argc != 3) {
+		exit(1);
+	}
+
+	int src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1) {
+		perror("Error openning file");
+		exit(EXIT_FAILURE);
+	}
+
+	int src_fd_read = open(argv[1], O_RDONLY);
+        if (src_fd == -1) {
+                perror("Error openning file");
+                exit(EXIT_FAILURE);
+        }
+
+	int dst_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (dst_fd == -1) {
+                perror("Error openning file");
+                exit(EXIT_FAILURE);
+        }
+
+	int cur_pos = 0;
+	char buffer[4096];
+	int bytes_passed = 0;
+
+	while (true) {
+		bytes_passed = cur_pos;
+		cur_pos = lseek(src_fd, 0, SEEK_HOLE);
+		bytes_passed = cur_pos - bytes_passed;
+		if (cur_pos == -1) {
+			if (errno == ENXIO)
+				break;
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+
+		while (bytes_passed > 0) {
+			if (read(src_fd_read, buffer, (bytes_passed > 4096 ? 4096 : bytes_passed)) == -1) {
+				perror("Error");
+	      			exit(EXIT_FAILURE);
+			}
+			if (write(dst_fd, buffer, (bytes_passed > 4096 ? 4096 : bytes_passed)) == -1) {
+				perror("Error");
+	        	        exit(EXIT_FAILURE);
+			}
+			bytes_passed -= (bytes_passed > 4096 ? 4096 : bytes_passed);
+		}
+
+		bytes_passed = cur_pos;
+		cur_pos = lseek(src_fd, cur_pos, SEEK_DATA);
+		bytes_passed = cur_pos - bytes_passed;
+		if (cur_pos == -1) {
+                        if (errno == ENXIO)
+                                break;
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+		if (lseek(src_fd_read, bytes_passed, SEEK_CUR) == -1) {
+			perror("Error");
+                        exit(EXIT_FAILURE);
+		}
+		if (lseek(dst_fd, bytes_passed, SEEK_CUR) == -1) {
+			perror("Error");
+                        exit(EXIT_FAILURE);
+		}
+	}
+
+	close(src_fd);
+	close(dst_fd);
+}
